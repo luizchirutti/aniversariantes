0f15cd9439fa5035733e221557879ee111379c24
Luiz Carlos Chirutti <37088740+luizchirutti@users.noreply.github.com>
2022-08-22 17:13:51 -0300

    Initial commit

---
+++ src/webparts/aniversariantes/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IAniversariantesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'AniversariantesWebPartStrings' {
+  const strings: IAniversariantesWebPartStrings;
+  export = strings;
+}
